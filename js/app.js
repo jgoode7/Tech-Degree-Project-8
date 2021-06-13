07de5feacbcfd853239b8ee0dedc1e3ea1d39d11
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-06-13 09:26:28 -0700

    everything working, basic mockup matched

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Global Variables
 let employee = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
-const gridContainer = document.querySelector(".grid-conatiner");
+const urlAPI = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US';
+const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector("overlay");
 const modalConatainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
@@ -9,29 +9,26 @@ const modalClose = document.querySelector(".modal-close");
 // Fetch from API
 fetch(urlAPI)
     .then(res => res.json())
-    .then(console.log(res))
     .then(res => res.results)
     .then(displayEmployees)
     .catch(err => console.log(err));
 
 // Function for Employee Display
-function displayEmployees(employeeData) {
-    
+function displayEmployees(employeeData) {  
     employees = employeeData;
-
     let employeeHTML = '';
 
     employees.forEach((employee, index) => {
         let name = employee.name;
         let email = employee.email;
-        let city = employee.city;
+        let city = employee.location.city;
         let picture = employee.picture;
 
         employeeHTML += `
             <div class="card" data-index="${index}">
-                <img class="avatar" src="$(picture.large)" />
+                <img class="avatar" src="${picture.large}" />
                 <div class="text-container">
-                    <h2 class="name>${name.first} ${name.last}"></h2>
+                    <h2 class="name">${name.first} ${name.last}</h2>
                     <p class="email">${email}</p>
                     <p class="address">${city}</p>
                 </div>
@@ -43,13 +40,13 @@ function displayEmployees(employeeData) {
 }
 
 // Function for Modal Display
-function displayModal(index) { `
+function displayModal(index) { 
 
     let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
 
     let date = new Date(dob.date);
 
-    const modalHTML = 
+    const modalHTML = `
         <img class="avatar" src="${picture.large}" />
         <div class="text-conatiner">
             <h2 class="name">${name.first} ${name.last}</h2>
@@ -57,7 +54,7 @@ function displayModal(index) { `
             <p class="address">${city}</p>
             <hr />
             <p>${phone}</p>
-            <p class="address">${street}, ${state} ${postcode}</p>
+            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
             <p>Birthday:
 ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
