c3ca87b392e4b5f4c8088e083e3221f28979cd8b
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-06-13 15:14:42 -0700

    fixing broken modal

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 let employee = [];
 const urlAPI = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US';
 const gridContainer = document.querySelector(".grid-container");
-const overlay = document.querySelector("overlay");
+const overlay = document.querySelector(".overlay");
 const modalConatainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
 
